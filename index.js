068eca77c95504f9c5db250d05e63a932056cf4c
himanshu kumar <himanshu.kumar@fluper.in>
2019-02-19 10:50:33 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var socketClient;
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
+
+
+io.on('connection', function (socket) {
+    socketClient=socket;
+
+    socket.on('chat message', function (msg) {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
